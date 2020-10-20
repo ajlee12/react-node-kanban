6fa02841825ea285b525d3de151f5eb33e7aeec7
ajlee12 <rach.on@gmail.com>
2020-10-19 22:03:49 -0700

    Add more Redux related files.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
       <h1>Hello World!</h1>
     </div>
   );
-}
+};
 
 export default App;

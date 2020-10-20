4e2d130d8f3e0eaea6d33c4f83fa0dafd26ff28e
ajlee12 <rach.on@gmail.com>
2020-10-20 10:20:52 -0700

    Have List dynamically render respective list contents.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,23 +8,34 @@ const mapStateToProps = (store: AppState) => ({
   PhoneScreen: store.cards.PhoneScreen,
 });
 
-interface ListProps { 
-  listTitle: string,
-  Applied?: CardContents[],
-  PhoneScreen?: CardContents,
+interface ListPropsKeys {
+  [key: string]: any,
+};
+
+interface ListProps extends ListPropsKeys { 
+  // listTitle: string,
+  Applied: CardContents[],
+  PhoneScreen: CardContents[],
 };
 
 //{listTitle, Applied, PhoneScreen }
 
 const List = (props: ListProps) => {
+  const listTitle = props.listTitle;
   return (
     <div style={styles}>
       <h2>{props.listTitle}</h2>
       This is a List component.
-      {console.log(props.Applied)}
-      {props.Applied?.map((card) => {
-        // return `${card.name}, ${card.comments}`
-        return <Card name={card.name} comments={card.comments} />
+      {/* {console.log('props in List:',props)} */}
+      {console.log('props[listTitle]:',props[listTitle])}
+      {props[listTitle].map((card: CardContents) => {
+        return ( 
+          <Card 
+            name={card.name}
+            comments={card.comments}
+            listTitle={props.listTitle}
+          />
+        );
       })}
       {/* <Card /> */}
     </div>

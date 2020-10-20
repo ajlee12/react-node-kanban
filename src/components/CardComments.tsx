93bcc15bc96647dca638d94af567adebf6173e68
ajlee12 <rach.on@gmail.com>
2020-10-20 10:51:12 -0700

    Render input box in CardComments.

--- src/components/CardComments.tsx
+++ src/components/CardComments.tsx
@@ -4,6 +4,10 @@ const CardComments: FC = () => {
   return (
     <div>
       This a CardComments component.
+      <form>
+        <h4>Add comments:</h4>
+        <input type="text" placeholder='Comments..'></input>
+      </form>
     </div>
   );
 };

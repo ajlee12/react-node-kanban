10840f87f00be400561ecaf8b0b8e17505b0f1a9
ajlee12 <rach.on@gmail.com>
2020-10-20 11:10:40 -0700

    Add handleChange in comment box.

--- src/components/CardComments.tsx
+++ src/components/CardComments.tsx
@@ -1,12 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 const CardComments: FC = () => {
+  const [input, setInputValue] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // Prevents reload upon submit.
+    e.preventDefault();
+
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
       This a CardComments component.
-      <form>
+      <form >
         <h4>Add comments:</h4>
-        <input type="text" placeholder='Comments..'></input>
+        <input type="text" placeholder='Comments..' value={input} onChange={handleChange} />
       </form>
     </div>
   );

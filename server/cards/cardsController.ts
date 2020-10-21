ea5ba58f32b508f1c4640a43ea81944cef83a401
ajlee12 <rach.on@gmail.com>
2020-10-21 15:02:33 -0700

    Add simple resp from server.

--- server/cards/cardsController.ts
+++ server/cards/cardsController.ts
@@ -3,8 +3,12 @@ import { Request, Response, NextFunction } from 'express';
 class CardsController {
 
   addCard(req: Request, res: Response, next: NextFunction) {
-    // const { name, comments } = req.body;
-    console.log('*** inside CardsController.addCard ***')
+    const { name, comments } = req.body;
+    console.log('*** inside CardsController.addCard ***', `name: ${name}, comments: ${comments}`);
+    console.log('req.body:', req.body)
+
+
+    return next();
   }
 }
 

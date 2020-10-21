5f1b8feeeaa171046ffea697c3bbda8850d2d93f
ajlee12 <rach.on@gmail.com>
2020-10-21 16:23:49 -0700

    Configure express error handlers.

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import cards from './cards/cardsRoutes';
 
@@ -7,5 +7,25 @@ const app = express();
 app.use(express.json());
 
 app.use('/cards', cards);
+
+/*
+ * Bad route handler
+ */
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err = new Error('Not Found');
+  
+  res.status(404).json(err);
+});
+
+/*
+ * Express.next(err) global error handler
+ */
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const errMessage = `Error details: ${err}`;
+
+  console.error(`Error occurred in ${res.locals.errLocation}; ${errMessage}`);
+
+  return res.status(500).json({ message: errMessage });
+});
 
 export default app;
